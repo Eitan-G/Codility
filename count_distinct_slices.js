7fc7df1e695c71f66263621d02fcad82d4c0599d
Eitan <eitan-g@users.noreply.github.com>
2019-03-31 12:00:36 -0700

    Add guard clause for 1e9 slices

--- count_distinct_slices.js
+++ count_distinct_slices.js
@@ -1,4 +1,4 @@
-// 100% correctness, 80% performance - 90% overall
+// 100% correctness, 100% performance - 100% overall
 
 function solution(M, A) {
     let end = 0, slices = 0
@@ -7,6 +7,7 @@ function solution(M, A) {
         while (end < A.length && !dict[A[end]]) {
             dict[A[end]] = 1
             slices += end - start + 1
+            if (slices >= 1e9) { return 1e9 }
             end++
         }
         dict[A[start]] = dict[A[start]] - 1
