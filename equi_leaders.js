7bcb45393d93d29a7a73fe048e5d8b6c68e31894
Eitan <eitan-g@users.noreply.github.com>
2019-03-31 11:57:51 -0700

    Solve EquiLeaders a different wat

--- equi_leaders.js
+++ equi_leaders.js
@@ -1,27 +1,48 @@
+const incrementCounter = (n, counter = {}) => ({ ...counter, [n]: ~~counter[n] + 1 })
+
 const solution = A => {
-    const len = A.length, prefixLeaders = [], lookup = {}
-    let equiLeaders = 0, count = {}
+    const N = A.length, counts = [], lookup = {}
+    let leader = A[0], equiLeaders = 0
 
-    for (let i = 0; i < len; i++) {
-        const half = (i + 1) / 2, previousLeader = prefixLeaders[i - 1]
-        lookup[A[i]] = ~~lookup[A[i]] + 1
+    for (let i = 0; i < N; i++) {
+        lookup[i] = ~~lookup[i] + 1
+        counts.push(incrementCounter(A[i], counts[i - 1]))
+    }
+    const last = counts[counts.length - 1]
 
-        if (lookup[A[i]] > half) {
-            prefixLeaders.push(A[i])
-        } else if (lookup[previousLeader] > half) {
-            prefixLeaders.push(previousLeader)
-        } else {
-            prefixLeaders.push(null)
+    for (let i = 0; i < N - 1; i++) {
+        if (counts[i][A[i]] > (i + 1) / 2) {
+            leader = A[i]
+        } else if (counts[i][leader] <= (i + 1) / 2) {
+            continue
+        }
+        if (last[leader] - counts[i][leader] > (N - 1 - i) / 2 ) {
+            equiLeaders++
         }
     }
+    return equiLeaders
+}
 
-    for (let i = 0; i < len; i++) {
-        count[A[i]] = ~~count[A[i]] + 1
-        let current = prefixLeaders[i]
-        const isPreLeader = count[current] > (i + 1) / 2,
-              isPostLeader = lookup[current] - count[current] > (len - 1 - i) / 2
+// Times out??? 100% correctness, 75% performance - 88% overall
+const solution = A => {
+    const N = A.length, counts = [], lookup = {}
+    let leader = A[0], equiLeaders = 0
 
-        if ( isPreLeader && isPostLeader ) {
+    for (let i = 0; i < N; i++) {
+        lookup[i] = ~~lookup[i] + 1
+        const counter = { ...counts[i - 1] }
+        counter[A[i]] = ~~counter[A[i]] + 1
+        counts.push(counter)
+    }
+    const last = counts[counts.length - 1]
+
+    for (let i = 0; i < N - 1; i++) {
+        if (counts[i][A[i]] > (i + 1) / 2) {
+            leader = A[i]
+        } else if (counts[i][leader] <= (i + 1) / 2) {
+            continue
+        }
+        if (last[leader] - counts[i][leader] > (N - 1 - i) / 2 ) {
             equiLeaders++
         }
     }
